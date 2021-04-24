57720682066946015ba28a2a9bac6fd554f9e8c2
Error4ik <fearerror@yandex.ru>
2021-04-24 19:25:38 +0300

    Dependency injection with setter.

--- src/main/java/com/voronin/spring/Person.java
+++ src/main/java/com/voronin/spring/Person.java
@@ -5,6 +5,9 @@ import com.voronin.spring.interfaces.Pet;
 public class Person {
     private Pet pet;
 
+    public Person() {
+    }
+
     public Person(Pet pet) {
         this.pet = pet;
     }
@@ -12,6 +15,10 @@ public class Person {
     public void callYourPet() {
         System.out.println("Pet voice!");
         this.pet.say();
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
     }
 
     public Pet getPet() {
