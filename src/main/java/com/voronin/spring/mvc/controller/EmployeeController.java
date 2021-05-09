abcdd3eddb8e70a5cdd7838197f45912c843ef2e
Error4ik <fearerror@yandex.ru>
2021-05-09 19:22:30 +0300

    Controller request mapping.

--- src/main/java/com/voronin/spring/mvc/controller/EmployeeController.java
+++ src/main/java/com/voronin/spring/mvc/controller/EmployeeController.java
@@ -5,14 +5,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
-public class ShowEmployeeDetailsController {
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @RequestMapping("/")
+    public String showIndexPage() {
+        return "index";
+    }
+
+    @RequestMapping("/ask-details")
+    public String askEmployeeDetails() {
+        return "ask-employee-details-view";
+    }
 
     @RequestMapping("/show-details")
     public String showEmployeeDetails(@RequestParam(name = "employeeName") String empName, Model model) {
-        String name = "Hello - " +  empName + "!!!";
+        String name = "Hello - " + empName + "!!!";
         model.addAttribute("nameAttribute", name);
         return "show-employee-details-view";
     }
