018f8b4adb6c970e33e4f2e1038993cc98e89f79
Error4ik <fearerror@yandex.ru>
2021-05-16 22:53:31 +0300

    Added exception handler.

--- src/main/java/com/voronin/spring/rest/controller/EmployeeController.java
+++ src/main/java/com/voronin/spring/rest/controller/EmployeeController.java
@@ -1,11 +1,12 @@
 package com.voronin.spring.rest.controller;
 
 import com.voronin.spring.rest.entity.Employee;
+import com.voronin.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.voronin.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.voronin.spring.rest.services.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +27,17 @@ public class EmployeeController {
 
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable int id) {
-        return this.employeeService.getEmployee(id);
+        Employee employee = this.employeeService.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException(String.format("There is no Employee with id %s in Database", id));
+        }
+        return employee;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handlerException(NoSuchEmployeeException noSuchEmployeeException) {
+        return new ResponseEntity<>(
+                new EmployeeIncorrectData(noSuchEmployeeException.getMessage()),
+                HttpStatus.NOT_FOUND);
     }
 }
